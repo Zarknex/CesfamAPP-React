2b59dd55e830d78ce9816d3dfe36a5d91bcf96c1
Matias Donoso <rmttck@gmail.com>
2022-05-14 10:25:11 -0400

    tailwind installed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,10 +1,6 @@
 const alert = () => {
   return (
-    <div
-      className={`${
-        alert.error ? "from-red-400 to-red-600" : "from-sky-400 to-sky-600"
-      } bg-gradient-to-br text-center p-3 rounded-xl uppercase text-white font-bold text-sm my-10`}
-    >
+    <div className={` text-center p-3 rounded-xl uppercase text-sm my-10`}>
       {alert.msg}
     </div>
   );
